38d8554c406131d1e1bedd0a04074ea81ffaa87e
wafa <wafa.khalifa42@gmail.fr>
2018-03-27 12:05:26 +0200

    Commit27/03/2018

--- src/main/java/com/example/annuaire/carnet/Carnet.java
+++ src/main/java/com/example/annuaire/carnet/Carnet.java
@@ -1,76 +1,119 @@
 package com.example.annuaire.carnet;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 public class Carnet {
-   private int id;
-   private Civilite civilite;
-   private String nom,prenom,dateDeNaissance,tel,email,cp,ville;
-public int getId() {
-	return id;
-}
-public Carnet() {}
-public Carnet(int id, Civilite civilite, String nom, String prenom, String dateDeNaissance, String tel, String email,
-		String cp, String ville) {
-	super();
-	this.id = id;
-	this.civilite = civilite;
-	this.nom = nom;
-	this.prenom = prenom;
-	this.dateDeNaissance = dateDeNaissance;
-	this.tel = tel;
-	this.email = email;
-	this.cp = cp;
-	this.ville = ville;
-}
-public void setId(int id) {
-	this.id = id;
-}
-public Civilite getCivilite() {
-	return civilite;
-}
-public void setCivilite(Civilite civilite) {
-	this.civilite = civilite;
-}
-public String getNom() {
-	return nom;
-}
-public void setNom(String nom) {
-	this.nom = nom;
-}
-public String getPrenom() {
-	return prenom;
-}
-public void setPrenom(String prenom) {
-	this.prenom = prenom;
-}
-public String getDateDeNaissance() {
-	return dateDeNaissance;
-}
-public void setDateDeNaissance(String dateDeNaissance) {
-	this.dateDeNaissance = dateDeNaissance;
-}
-public String getTel() {
-	return tel;
-}
-public void setTel(String tel) {
-	this.tel = tel;
-}
-public String getEmail() {
-	return email;
-}
-public void setEmail(String email) {
-	this.email = email;
-}
-public String getCp() {
-	return cp;
-}
-public void setCp(String cp) {
-	this.cp = cp;
-}
-public String getVille() {
-	return ville;
-}
-public void setVille(String ville) {
-	this.ville = ville;
-}
-   
+
+	private int id;
+	@NotNull // @NotNull(message="Il faut remplir les champs vides")
+	private Civilite civilite;
+	// @NotBlank
+	@Pattern(regexp = "(?i)[a-z]{2,50}", message = "{com.formation.annuaire.constraint.nom.message}") // i ignorer la
+																										// casse
+	private String nom;
+	// @NotBlank
+	@Pattern(regexp = "(?i)[a-z\\- ]{2,100}", message = "{com.formation.annuaire.constraint.nom.message}")
+	private String prenom;
+	@NotBlank
+	private String dateDeNaissance;
+	@Pattern(regexp = "^(\\+[0-9]|[0]{2})[0-9] {6,14}(?:x.+)?$", message = "{com.formation.annuaire.constraint.tel.message}")
+	private String tel;
+	@Pattern(regexp = "\\b[A-Z0-9._%-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b", message = "{com.formation.annuaire.constraint.email.message}")
+	private String email;
+	@Pattern(regexp = "^(([0-8][0-9])|(9[0-5])|(2[ab]))[0-9]{3}$", message = "{com.formation.annuaire.constraint.cp.message}")
+	private String cp;
+	@Pattern(regexp = "(?i)[a-z -] {1,45}", message = "{com.formation.annuaire.constraint.nom.message}")
+	private String ville;
+
+	public int getId() {
+		return id;
+	}
+
+	public Carnet() {
+	}
+
+	public Carnet(int id, Civilite civilite, String nom, String prenom, String dateDeNaissance, String tel,
+			String email, String cp, String ville) {
+		super();
+		this.id = id;
+		this.civilite = civilite;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.dateDeNaissance = dateDeNaissance;
+		this.tel = tel;
+		this.email = email;
+		this.cp = cp;
+		this.ville = ville;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Civilite getCivilite() {
+		return civilite;
+	}
+
+	public void setCivilite(Civilite civilite) {
+		this.civilite = civilite;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getDateDeNaissance() {
+		return dateDeNaissance;
+	}
+
+	public void setDateDeNaissance(String dateDeNaissance) {
+		this.dateDeNaissance = dateDeNaissance;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCp() {
+		return cp;
+	}
+
+	public void setCp(String cp) {
+		this.cp = cp;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
 }
--- src/main/java/com/example/annuaire/carnet/CarnetController.java
+++ src/main/java/com/example/annuaire/carnet/CarnetController.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,8 +24,7 @@ public class CarnetController {
 			new Carnet(2, Civilite.MADAME, "Ullmann", "Liv", "1938-12-16", "0101010101", "1-16-5", "22", "Tokyo"),
 			new Carnet(3, Civilite.MONSIEUR, "Lynch", "David", "1946-01-20", "0202020202", "423 Fire Terrace", "59801",
 					"Missoula")));
-    // Comment From getALL method
-
+	// Comment From getALL method
 
 	// other comment
 	// And other comment
@@ -32,46 +34,64 @@ public class CarnetController {
 		return listeCarnets;
 	}
 
-	@GetMapping("/carnetshtml")// @RequestMapping(value="/carnets", method=RequestMethod.GET)
+	@GetMapping("/carnetshtml") // @RequestMapping(value="/carnets", method=RequestMethod.GET)
 	public ModelAndView getAll() {
 		// model.addAttribute("liste",listeCarnets);
 		ModelAndView mav = new ModelAndView("pages/carnets");
 		mav.addObject("liste", listeCarnets);
 		return mav;
 	}
-	@RequestMapping(value="/carnets/{id}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/carnets/{id}", method = RequestMethod.GET)
 	public Carnet getById(@PathVariable Integer id) {
-    	for (Carnet c : this.listeCarnets) {
-    		if (((Integer)c.getId()).equals(id)){
-    			return c;
-    		}
-    	}
-    	return null;
+		for (Carnet c : this.listeCarnets) {
+			if (((Integer) c.getId()).equals(id)) {
+				return c;
+			}
+		}
+		return null;
 	}
-	@RequestMapping(value="/carnets/{id}", method=RequestMethod.DELETE)
+
+	@RequestMapping(value = "/carnets/{id}", method = RequestMethod.DELETE)
 	public void DelById(@PathVariable Integer id) {
-		this.listeCarnets.removeIf(p-> ((Integer)p.getId()).equals(id));
+		this.listeCarnets.removeIf(p -> ((Integer) p.getId()).equals(id));
 	}
-	
-	//@RequestMapping(value="/carnets") en brut
-	/*@PostMapping("/carnets") 
-	public void add(@RequestBody Carnet carnet)
-	{
+
+	// @RequestMapping(value="/carnets") en brut
+	@PostMapping("/carnets") // aJOUT jSON
+	public void addjson(@RequestBody Carnet carnet) {
 		this.listeCarnets.add(carnet);
 	}
-	*/
-	@RequestMapping(value="/formulaire",method=RequestMethod.POST)
-	public void add(@ModelAttribute Carnet carnet)
-	{ 
-		carnet.setId(listeCarnets.size()+1);
+
+	// Ajout de carnet par formulaire en post
+	@RequestMapping(value = "/formulaire", method = RequestMethod.POST)
+	// public void add(@ModelAttribute Carnet carnet)
+	public ModelAndView add(@Validated Carnet carnet, BindingResult br) {
+		if (br.hasErrors()) {
+			return new ModelAndView("pages/formulairecarnet.html").addObject("carnet", carnet);
+		}
+
+		carnet.setId(listeCarnets.size() + 1);
 		this.listeCarnets.add(carnet);
+		return new ModelAndView("pages/carnetResultAjout.html").addObject("carnet", carnet);
 	}
-	
-	@GetMapping(value="/formulaire")
-	public ModelAndView form()
-	{
-		ModelAndView mav = new ModelAndView("pages/formulairecarnet");		
+
+	@GetMapping(value = "/formulaire")
+	public ModelAndView form() {
+		ModelAndView mav = new ModelAndView("pages/formulairecarnet");
+		mav.addObject("carnet", new Carnet());// en version th field pour validation formulaire
 		return mav;
 	}
-	
+
+	@RequestMapping(value = "/modifier/{id}", method = RequestMethod.PUT)
+	public Carnet update(@RequestBody Carnet carnet, @PathVariable Integer id) {
+		for (Integer i = 0; i < this.listeCarnets.size(); i++) {
+			Carnet c = this.listeCarnets.get(i);
+			if (((Integer) c.getId()).equals(id)) {
+				this.listeCarnets.set(i, carnet);
+				break;
+			}
+		}
+		return null;
+	}
 }
